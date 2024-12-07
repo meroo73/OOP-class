e4e3103e245ea10b7d5d00d58b7ed0d155747738
Ameer <79278779+meroo73@users.noreply.github.com>
2024-12-07 12:55:31 +0100

    Update a5.cpp

--- a5.cpp
+++ a5.cpp
@@ -1,10 +1,5 @@
-/***********************************************************************
-Matr.-Nr:                     3140507
-Nachname/Surname:             Alaghbari
-Vorname/Given name:           Ameer Adin
-Uni-Email:                    ameer.alaghbari@stud.uni-due.de
-Studiengang/Course of studis: B.Sc ISE CE (Software)
-***********************************************************************/
+
+
 
 #include <iostream>
 #include <string>
